11dd6a73a1d344321c835ec9b39f4ca0b6e53953
gsalicruu <146372806+gsalicruu@users.noreply.github.com>
2025-05-20 20:59:22 +0200

    Update DDL_DML.sql
    Errores de sintaxis corregidos

--- DDL_DML.sql
+++ DDL_DML.sql
@@ -4,7 +4,7 @@ CREATE TABLE Tareas (
     Salas_ID INTEGER,
     Rol_ID INTEGER,
     FOREIGN KEY (Salas_ID) REFERENCES Salas(ID),
-    FOREIGN KEY (Rol_ID) REFERENCES Roles(ID)
+    FOREIGN KEY (Rol_ID) REFERENCES Roles(ID)
 );
 
 CREATE TABLE Personajes (
@@ -12,63 +12,60 @@ CREATE TABLE Personajes (
     Sombrero BOOLEAN,
     Traje BOOLEAN,
     Rol_ID INTEGER,
-    FOREIGN KEY (Rol_ID) REFERENCES Roles(ID)
+    FOREIGN KEY (Rol_ID) REFERENCES Roles(ID)
 );
 
 CREATE TABLE Muertes (
-ID INTEGER UNIQUE NOT NULL PRIMARY KEY AUTOINCREMENT,
-Victima INTEGER,
-Asesino INTEGER,
-Salas_ID INTEGER,
-FOREIGN KEY (Victima) REFERENCES  Personajes(ID),
-FOREIGN KEY (Asesino) REFERENCES  Personajes(ID),
-FOREIGN KEY (Salas_ID) REFERENCES Salas(ID)
+    ID INTEGER UNIQUE NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Victima INTEGER,
+    Asesino INTEGER,
+    Salas_ID INTEGER,
+    FOREIGN KEY (Victima) REFERENCES Personajes(ID),
+    FOREIGN KEY (Asesino) REFERENCES Personajes(ID),
+    FOREIGN KEY (Salas_ID) REFERENCES Salas(ID)
 );
 
 CREATE TABLE Salas (
-ID INTEGER UNIQUE NOT NULL PRIMARY KEY,
-Ubicacion TEXT,
-Capacidad INTEGER
+    ID INTEGER UNIQUE NOT NULL PRIMARY KEY,
+    Ubicacion TEXT,
+    Capacidad INTEGER
 );
 
-CREATE TABLE Salas-Salas (
-ID_1 INTEGER,
-ID_2 INTEGER,
-PRIMARY KEY (ID_1, ID_2),
-FOREING KEY (ID_1) REFERENCES Salas(ID),
-FOREING KEY (ID_2) REFERENCES Salas(ID)
+CREATE TABLE Salas_Salas (
+    ID_1 INTEGER,
+    ID_2 INTEGER,
+    PRIMARY KEY (ID_1, ID_2),
+    FOREIGN KEY (ID_1) REFERENCES Salas(ID),
+    FOREIGN KEY (ID_2) REFERENCES Salas(ID)
 );
 
 CREATE TABLE Roles (
-  ID INTEGER UNIQUE NOT NULL PRIMARY KEY,
-  Potenciador TEXT
+    ID INTEGER UNIQUE NOT NULL PRIMARY KEY,
+    Potenciador TEXT
 );
 
-INSERT INTO Roles (ID, Potenciador) VALUES(
+INSERT INTO Roles (ID, Potenciador) VALUES
 (1, "Velocidad"),
 (2, "Daño"),
 (3, "Disfraz"),
-(4 "Inteligencia")
-);
+(4, "Inteligencia");
 
-INSERT INTO Salas (ID, Ubicacion, Capacidad) VALUES (
+INSERT INTO Salas (ID, Ubicacion, Capacidad) VALUES
 (1, "este", 5),
-(2, "oeste", 3,),
+(2, "oeste", 3),
 (3, "este", 4),
 (4, "oeste", 6),
-(5, "oeste", 6)
-);
+(5, "oeste", 6);
 
-INSERT INTO Tareas (ID, Estado, Salas_ID, Rol_ID) VALUES (
+INSERT INTO Tareas (ID, Estado, Salas_ID, Rol_ID) VALUES
 (1, false, 1, 2),
 (2, false, 1, 2),
 (3, false, 4, 1),
 (4, false, 3, 4),
 (5, false, 2, 3),
-(6, false, 2, 1)
-);
+(6, false, 2, 1);
 
-INSERT INTO Salas-Salas (ID_1, ID_2) VALUES (
+INSERT INTO Salas_Salas (ID_1, ID_2) VALUES
 (1, 2),
 (2, 1),
 (3, 1),
@@ -78,5 +75,4 @@ INSERT INTO Salas-Salas (ID_1, ID_2) VALUES (
 (5, 4),
 (4, 5),
 (2, 5),
-(5, 2)
-);
+(5, 2);
